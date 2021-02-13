d4cff68645bdc64bb0ff1bbcd76e8457c7320242
Anton_Poludnitsin <anton.poludnitsin@spglobal.com>
2021-02-13 08:05:38 +0400

    add progression game

---
+++ src/games/brain-progression.js
@@ -0,0 +1,53 @@
+import readlineSync from 'readline-sync';
+
+let name = null;
+let result = 0;
+const lengthProgression = 10;
+let correctAnswer = null;
+const getRandomNumber = (number) => Math.floor(Math.random() * number);
+const getProgression = () => {
+  const firstItem = getRandomNumber(30);
+  const diffItem = getRandomNumber(30);
+  const missingItem = getRandomNumber(lengthProgression);
+  let count = firstItem;
+  const progression = [firstItem];
+  for (let i = 1; i < lengthProgression; i += 1) {
+    count += diffItem;
+    if (i === missingItem) {
+      correctAnswer = count;
+      progression.push('..');
+    } else {
+      progression.push(count);
+    }
+  }
+  return progression.join(' ');
+};
+
+const getQuestion = () => {
+  console.log(`Question: ${getProgression()}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (+answer === correctAnswer) {
+    console.log('Correct!');
+    result += 1;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+  }
+};
+
+export const startProgressionGame = () => {
+  console.log('Welcome to the Brain Games!');
+  name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('What number is missing in the progression?');
+  getQuestion();
+  if (result === 1) {
+    getQuestion();
+  }
+  if (result === 2) {
+    getQuestion();
+  }
+  if (result === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { startEvenGame } from './games/brain-even.js';
 import { startCalcGame } from './games/brain-calc.js';
 import { startGcdGame } from './games/brain-gcd.js';
+import { startProgressionGame } from './games/brain-progression.js';
 
 export const startGame = (title) => {
   if (title === 'even') {
@@ -11,5 +12,8 @@ export const startGame = (title) => {
   }
   if (title === 'gcd') {
     startGcdGame();
+  }
+  if (title === 'progression') {
+    startProgressionGame();
   }
 };
